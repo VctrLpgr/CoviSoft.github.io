4dc4b631f1c4bb0ffd5b163b901a2aafde13f2a4
VctrLpgr <84886261+VctrLpgr@users.noreply.github.com>
2021-05-26 21:27:17 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyCZ7nci-rNLlqywUmwwrkOEzydzHp3OSgQ",
+    authDomain: "pruebaghvictor.firebaseapp.com",
+    projectId: "pruebaghvictor",
+    storageBucket: "pruebaghvictor.appspot.com",
+    messagingSenderId: "112030187937",
+    appId: "1:112030187937:web:156bf35a2cf77df3168be0",
+    measurementId: "G-8512L4TZD6"
+  });
